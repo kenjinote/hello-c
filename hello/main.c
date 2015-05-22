360a62b9955eb551e0a1bc7bc3be03984d4f8063
kenji <kenji@LAZY-MAC.local>
2015-05-22 23:11:51 +0900

    Initial Commit

---
+++ hello/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello
+//
+//  Created by kenji on 2015/05/22.
+//  Copyright (c) 2015年 kenji. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
